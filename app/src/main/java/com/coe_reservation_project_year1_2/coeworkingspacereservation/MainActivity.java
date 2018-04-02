76a2eec633579c00321c65688420a4b3dead3074
wuttiamornsak1999 <pornsukon@kkumail.com>
2018-04-02 23:43:34 +0700

    Initial commit

---
+++ app/src/main/java/com/coe_reservation_project_year1_2/coeworkingspacereservation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coe_reservation_project_year1_2.coeworkingspacereservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
